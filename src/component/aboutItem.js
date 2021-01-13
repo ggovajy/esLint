314be4460c4cb4b39662cec36926b4b2928c9c10
ggovajy <ggova@naver.com>
2021-01-14 08:59:34 +0900

    table append

--- src/component/aboutItem.js
+++ src/component/aboutItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
 export default class aboutItem extends Component {
     constructor(props) {
@@ -46,35 +47,55 @@ export default class aboutItem extends Component {
                 <span>{this.state.list.length}</span>
                 <label>조회 건수</label>
                 <span>{this.state.filterList.length}</span>
+                    <Table fixed>
+                    <Table.Header>
+                        <Table.Row>
+                        <Table.HeaderCell>ein</Table.HeaderCell>
+                        <Table.HeaderCell>strein</Table.HeaderCell>
+                        <Table.HeaderCell>name</Table.HeaderCell>
+                        <Table.HeaderCell>sub_name</Table.HeaderCell>
+                        <Table.HeaderCell>city</Table.HeaderCell>
+                        <Table.HeaderCell>state</Table.HeaderCell>
+                        <Table.HeaderCell>ntee_code</Table.HeaderCell>
+                        <Table.HeaderCell>raw_ntee_code</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
                 {this.state.filterList.map((item) =>(
-                <div style={{ paddingTop: 20}} key = {item.id}>
-                    <label>ein : </label>
-                    <span style={{ paddingRight: 20}}>{item.ein}</span>
-                    
-                    <label>strein : </label>
-                    <span style={{ paddingRight: 20}}>{item.strein}</span>
-                    
-                    <label>name : </label>
-                    <span style={{ paddingRight: 20}}>{item.name}</span>
-                    
-                    <label>sub_name : </label>
-                    <span style={{ paddingRight: 20}}>{item.sub_name}</span>
-                    
-                    <label>city : </label>
-                    <span style={{ paddingRight: 20}}>{item.city}</span>
-                    
-                    <label>state : </label>
-                    <span style={{ paddingRight: 20}}>{item.state}</span>
-                    
-                    <label>ntee_code : </label>
-                    <span style={{ paddingRight: 20}}>{item.ntee_code}</span>
-                    
-                    <label>raw_ntee_code : </label>
-                    <span style={{ paddingRight: 20}}>{item.raw_ntee_code}</span>
-                    
-                    <br/>
-                </div>
+
+                    <Table.Body>
+                        <Table.Row>
+                        <Table.Cell>{item.ein}</Table.Cell>
+                        <Table.Cell>{item.strein}</Table.Cell>
+                        <Table.Cell>{item.name}</Table.Cell>
+                        <Table.Cell>{item.sub_name}</Table.Cell>
+                        <Table.Cell>{item.city}</Table.Cell>
+                        <Table.Cell>{item.state}</Table.Cell>
+                        <Table.Cell>{item.ntee_code}</Table.Cell>
+                        <Table.Cell>{item.raw_ntee_code}</Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+
                 ))}
+
+                    <Table.Footer>
+                        <Table.Row>
+                        <Table.HeaderCell colSpan='3'>
+                            <Menu floated='right' pagination>
+                            <Menu.Item as='a' icon>
+                                <Icon name='chevron left' />
+                            </Menu.Item>
+                            <Menu.Item as='a'>1</Menu.Item>
+                            <Menu.Item as='a'>2</Menu.Item>
+                            <Menu.Item as='a'>3</Menu.Item>
+                            <Menu.Item as='a'>4</Menu.Item>
+                            <Menu.Item as='a' icon>
+                                <Icon name='chevron right' />
+                            </Menu.Item>
+                            </Menu>
+                        </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
                 </>
             </div>
         )
