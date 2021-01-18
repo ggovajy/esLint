a41ac34fd2910f800e072d23495fde45c6c2e4ae
daisy-yoojin <daisy.park95@gmail.com>
2021-01-18 17:15:44 +0900

    simple add | YJ

---
+++ pages/solomon03/Form.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Form extends Component{
+    render(){
+        return (
+            <div>
+                여기는 입력 폼
+            </div>
+        );
+    }
+
+}
+
+export default Form;
