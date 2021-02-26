f12da8b5a6eac0d4fc3addc48202e37877c4e835
ggovajy <ggova@naver.com>
2021-02-26 17:18:52 +0900

    sessionStorage kakaoLogin

--- pages/login.js
+++ pages/login.js
@@ -1,58 +1,87 @@
-// import axios from 'axios';
-// import { Router, useRouter } from 'next/router';
-// import React from 'react';
-// import { Form, Button } from 'semantic-ui-react';
-
-// function login() {
-//   const router = useRouter();
-//   function checklogin() {
-//     axios.post('/api/login').then((res) => {
-//       if (res.status === 200) {
-//         router.push('/admin');
-//       }
-//     });
-//   }
-//   return (
-//     <div>
-//       <Form>
-//         <Form.Field inline>
-//           <input placeholder="ID" />
-//         </Form.Field>
-//         <Form.Field inline>
-//           <input type="password" placeholder="PASSWORD" />
-//         </Form.Field>
-//         <Button onClick={checklogin}>Login</Button>
-//       </Form>
-//     </div>
-//   );
-// }
-
-// export default login;
-
-import React, { useState } from 'react'
+/* global kakao */
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { GoogleLogin } from 'react-google-login'
+import KakaoLogin from 'react-kakao-login'
 import { Modal } from '@material-ui/core'
 const SigninPage = () => {
   const [isLogin, setIsLogin] = useState(false)
   const [id, setId] = useState('')
   const [open, setOpen] = useState(false)
 
+  useEffect(() => {
+    if (window.sessionStorage.getItem('provider')) {
+      setId(
+        `${window.sessionStorage.getItem('provider')} 환영합니다 화면로그인`
+      )
+      setIsLogin(true)
+    }
+    const script = document.createElement('script')
+    script.async = true
+    script.src = 'http://developers.kakao.com/sdk/js/kakao.min.js'
+    document.head.appendChild(script)
+    script.onload = () => {
+      createKakaoButton()
+    }
+  }, [])
+
+  const createKakaoButton = () => {
+    console.log(window)
+    console.log(window.Kakao)
+    if (window.Kakao) {
+      const kakao = window.Kakao
+      // 중복 initialization 방지
+      if (!kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        kakao.init('e885afef28b28578b398f40f079ecbd4')
+      }
+    }
+  }
+
   const responseGoogle = (response) => {
+    setOpen(false)
     if (response.tokenId) {
       setId(
         `${
           response.profileObj.familyName + response.profileObj.givenName
-        } 환영합니다`
+        } 환영합니다 google`
       )
       setIsLogin(true)
     } else {
       setIsLogin(false)
     }
+    doSignUp(response.profileObj.familyName, response.profileObj.givenName)
+  }
+  const responseKakao = (response) => {
+    // for (const i in response.profile) {
+    //   console.log(`key = ${i} value === ${response.profile[i]}`)
+    // }
+    if (response.profile.properties) {
+      setId(`${response.profile.properties.nickname}님 환영합니다 Kakao`)
+      setIsLogin(true)
+      doSignUp(
+        response.profile.properties.nickname.slice(0, 1),
+        response.profile.properties.nickname.slice(1)
+      )
+    } else {
+      setIsLogin(false)
+    }
     setOpen(false)
   }
-
+  const doSignUp = (fist, second) => {
+    console.log(`${fist} 성 이름  ===== ${second}`)
+    window.sessionStorage.setItem('id', fist)
+    window.sessionStorage.setItem('name', second)
+    window.sessionStorage.setItem('provider', fist + second)
+  }
+  const doSignDown = () => {
+    window.sessionStorage.setItem('id', '')
+    window.sessionStorage.setItem('name', '')
+    window.sessionStorage.setItem('provider', '')
+    console.log(window.sessionStorage.getItem('provider'))
+  }
   const logOut = () => {
+    doSignDown()
     setIsLogin(false)
   }
 
@@ -89,13 +118,24 @@ const SigninPage = () => {
             </ContainerTop>
             <ContainerBody>
               {!isLogin && (
-                <GoogleLogin
-                  clientId="34386281160-h3qbeu3vhqipa0dgfv3ei71e7cvu5gcv.apps.googleusercontent.com"
-                  buttonText="GoogleLogin"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
-                  cookiePolicy={'single_host_origin'}
-                />
+                <>
+                  <GoogleLogin
+                    clientId="34386281160-h3qbeu3vhqipa0dgfv3ei71e7cvu5gcv.apps.googleusercontent.com"
+                    buttonText="GoogleLogin"
+                    onSuccess={responseGoogle}
+                    onFailure={doSignDown}
+                    cookiePolicy={'single_host_origin'}
+                  />
+                  <KakaoLogin
+                    jsKey="e885afef28b28578b398f40f079ecbd4"
+                    onSuccess={responseKakao}
+                    onFailure={doSignDown}
+                    className="KakaoLogin"
+                    getProfile="true"
+                  >
+                    <p>카카오톡 로그인</p>
+                  </KakaoLogin>
+                </>
               )}
             </ContainerBody>
           </Container>
--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,45 +1,45 @@
-import { useRouter } from 'next/router';
-import { Menu } from 'semantic-ui-react';
+import { useRouter } from 'next/router'
+import { Menu } from 'semantic-ui-react'
 
 function Gnb() {
-  const router = useRouter();
-  let activeItem;
+  const router = useRouter()
+  let activeItem
 
   if (router.pathname === '/') {
-    activeItem = 'home';
+    activeItem = 'home'
   } else if (router.pathname === '/chart') {
-    activeItem = 'chart';
+    activeItem = 'chart'
   } else if (router.pathname === '/about') {
-    activeItem = 'about';
+    activeItem = 'about'
   } else if (router.pathname === '/login') {
-    activeItem = 'login';
+    activeItem = 'login'
   } else if (router.pathname === '/solomon01') {
-    activeItem = 'solomon01';
+    activeItem = 'solomon01'
   } else if (router.pathname === '/solomon02') {
-    activeItem = 'solomon02';
+    activeItem = 'solomon02'
   } else if (router.pathname === '/solomon03') {
-    activeItem = 'solomon03';
+    activeItem = 'solomon03'
   } else if (router.pathname === '/solomon04') {
-    activeItem = 'solomon04';
+    activeItem = 'solomon04'
   }
 
   function goLink(e, data) {
     if (data.name === 'home') {
-      router.push('/');
+      router.push('/')
     } else if (data.name === 'chart') {
-      router.push('/chart');
+      router.push('/chart')
     } else if (data.name === 'about') {
-      router.push('/about');
+      router.push('/about')
     } else if (data.name === 'login') {
-      router.push('/login');
+      router.push('/login')
     } else if (data.name === 'solomon01') {
-      router.push('/solomon01/solomon01');
+      router.push('/solomon01/solomon01')
     } else if (data.name === 'solomon02') {
-      router.push('/solomon02/solomon02');
+      router.push('/solomon02/solomon02')
     } else if (data.name === 'solomon03') {
-      router.push('/solomon03/solomon03');
+      router.push('/solomon03/solomon03')
     } else if (data.name === 'solomon04') {
-      router.push('/solomon04/solomon04');
+      router.push('/solomon04/solomon04')
     }
   }
 
@@ -55,45 +55,52 @@ function Gnb() {
         name="chart"
         active={activeItem === 'chart'}
         onClick={() => {
-          router.push('/chart');
+          const provider = window.sessionStorage.getItem('provider')
+          console.log(`${provider} 입니다`)
+          if (provider) {
+            router.push('/chart')
+          } else {
+            alert('로그인해주세요')
+            router.push('/login')
+          }
         }}
       />
       <Menu.Item
         name="login"
         active={activeItem === 'login'}
         onClick={() => {
-          router.push('/login');
+          router.push('/login')
         }}
       />
       <Menu.Item
         name="solomon01"
         active={activeItem === 'solomon01'}
         onClick={() => {
-          router.push('/solomon01/solomon01');
+          router.push('/solomon01/solomon01')
         }}
       />
       <Menu.Item
         name="solomon02"
         active={activeItem === 'solomon02'}
         onClick={() => {
-          router.push('/solomon02/solomon02');
+          router.push('/solomon02/solomon02')
         }}
       />
       <Menu.Item
         name="solomon03"
         active={activeItem === 'solomon03'}
         onClick={() => {
-          router.push('/solomon03/solomon03');
+          router.push('/solomon03/solomon03')
         }}
       />
       <Menu.Item
         name="solomon04"
         active={activeItem === 'solomon04'}
         onClick={() => {
-          router.push('/solomon04/solomon04');
+          router.push('/solomon04/solomon04')
         }}
       />
     </Menu>
-  );
+  )
 }
-export default Gnb;
+export default Gnb
